114f0db3ea7c0c9b3f67bac96bf2e220985ae5c2
whysiki <whysiki@proton.me>
2024-06-13 08:48:41 +0800

    1

--- vue_interface/src/axios.js
+++ vue_interface/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // 基础 URL 会被添加到所有请求的 URL 前面
-  baseURL: "http://47.115.43.139:8000",
+  baseURL: "http://localhost:8000",
   headers: {
     // 设置 Content-Type 请求头为 'application/json'
     "Content-Type": "application/json",
